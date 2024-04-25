858724a901fda6681110c0cf0f16518aac58c613
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-04-25 22:26:38 +0530

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState({});
   return (
-    <GoogleLogin
-      onSuccess={(credentialResponse) => {
-        const result = jwtDecode(credentialResponse.credential);
-        console.log(result);
-      }}
-      onError={() => {
-        alert("Login Failed");
-      }}
-    />
+    <>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          const result = jwtDecode(credentialResponse.credential);
+          console.log(result.picture);
+          setUser(result);
+        }}
+        onError={() => {
+          alert("Login Failed");
+        }}
+      />
+      <div>{user.email}</div>
+      <div
+        style={{
+          backgroundImage: `url(${user.picture})`,
+          height: "100px",
+          width: "100px",
+        }}
+      ></div>
+    </>
   );
 };
 
